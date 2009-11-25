331e50c1b9254e43cf3f14d0a974740f77c692bc
Peter Dodds <peter.dodds@gmail.com>
2009-11-25 18:59:00 +0000

    added empty LuaUtl file and updated Docs

---
+++ src/LuaUtl.cpp
@@ -0,0 +1,14 @@
+/// \file LuaUtl.cpp
+/// \date 2009 Nov 25
+/// \version
+/// \author
+/// \brief
+/// \note
+//-----------------------------------------------------------------------------
+
+#include "LuaUtl.hpp"
+
+namespace chain
+{
+
+}
